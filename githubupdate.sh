a8c02909bf2ea20003ea3e503ff82997ff6e2e8d
{ZehuaLee} <leezehua@outlook.com>
2017-02-17 15:21:19 +0900

    this is the automatically update @

--- githubupdate.sh
+++ githubupdate.sh
@@ -1,7 +1,7 @@
 # this is for automatically update script
 #! /bin/bash
 
-DATE=$(date -d +"%Y-%m-%d %T %z")
+DATE=$(date -d)
 git add --all
 git commit -m "this is the automatically update @ $DATE"
 git push origin master
