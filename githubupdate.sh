72b6e73cc4612575f68b6840ae0d4cf28734a760
{ZehuaLee} <leezehua@outlook.com>
2017-02-17 12:18:01 +0900

    this is the automatically update @

--- githubupdate.sh
+++ githubupdate.sh
@@ -1,7 +1,7 @@
 # this is for automatically update script
 #! /bin/bash
 
-DATE=$(date -d '1970-01-01 UTC 946684800 seconds' +"%Y-%m-%d %T %z")
+DATE=$(date -d +"%Y-%m-%d %T %z")
 git add --all
 git commit -m "this is the automatically update @ $DATE"
 git push origin master
